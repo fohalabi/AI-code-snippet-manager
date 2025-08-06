056da9c10ef09a5a3a3bd6b3f4d4aa8932a82f2b
fohalabi <ayomideabimbola79@gmail.com>
2025-08-06 09:50:23 -0700

    update the navbar

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -3,9 +3,9 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { RiCloseLargeFill } from 'react-icons/ri'
 
 const styles = {
-    link:"text-indigo-700 font-bold hover:text-white px-6 py-1 rounded-md transition-colors duration-700",
-    button: "bg-indigo-700 text-white px-2 py-1 rounded-full hover:bg-indigo-700 hover:shadow-md hover:scale-92 transition-all duration-300 whitespace-nowrap",
-    mobileButton: "bg-indigo-600 text-white px-4 py-3 rounded-full hover:bg-indigo-700 hover:scale-92 transtion-all duration-300 w-full whitespace-nowrap"
+    link:"text-black hover:bg-gray-200 hover:shadow-md px-6 py-1 rounded-md transition-colors duration-700",
+    button: "bg-white text-black px-2 py-1 rounded-md hover:shadow-full hover:scale-90 transition-all duration-300 whitespace-nowrap",
+    mobileButton: "bg-white text-black px-4 py-3 rounded-md hover:scale-90 transtion-all duration-300 w-full whitespace-nowrap"
 }
 
 const Navbar = () => {
@@ -15,15 +15,15 @@ const Navbar = () => {
         setIsMenuOpen(!isMenuOpen)
     }
   return (
-    <nav className='bg-transparent p-3 text-white relative top-4'>
+    <nav className='bg-white/70 backdrop-blur-sm border-b border-gray-200 text-black sticky top-0 z-50 w-full p-4'>
         <div className='flex justify-between items-center mx-auto container'>
 
             {/* Desktop navbar */}
-            <div className='hidden md:flex absolute left-1/2 transform -translate-x-1/2 mt-2 w-[90%]'>
-                <div className='bg-white/10 backdrop-blur-sm border border-white/20 rounded-full px-8 py-1 shadow-lg w-full'>
-                    <div className='flex justify-between item-center'>
-                        <div className='w-36 text-3xl text-indigo-700 hover:text-white'>
-                            <span className='font-bold mt-2'>code</span>
+            <div className='hidden md:flex w-full'>
+                <div className='w-full'>
+                    <div className='flex justify-between items-center'>
+                        <div className='w-36 text-3xl text-black'>
+                            <span className='font-bold'>code</span>
                             <span>vault</span>
                         </div>
                         <ul className='flex space-x-4 items-center'>
@@ -38,10 +38,10 @@ const Navbar = () => {
             </div>
             
             {/* Mobile navbar - expamdable container */}
-            <div className='md:hidden absolute left-1/2 transform -translate-x-1/2 mt-2 w-[90%]'>
-                <div className='bg-white/10 backdrop-blur-sm border border-white/20 rounded-full px-5 py-1 shadow-lg w-full'>
+            <div className='md:hidden w-full'>
+                <div className='w-full'>
                     <div className='flex justify-between items-center'>
-                        <div className='text-2xl text-indigo-700 hover:text-white'>
+                        <div className='text-2xl' >
                             <span className='font-bold'>code</span>
                             <span>vault</span>
                         </div>
@@ -59,10 +59,10 @@ const Navbar = () => {
                 <div className='flex flex-col space-y-4 text-center'>
 
                     <ul className='flex space-y-4 flex-col'>
-                        <li><a href="#" className="text-indigo-700 font-bold hover:text-white px-6 py-1 rounded-md transition-all duration-300 block">Features</a></li>
-                        <li><a href="#" className="text-indigo-700 font-bold hover:text-white px-6 py-1 rounded-md transition-all duration-300 block">Pricing</a></li>
-                        <li><a href="#" className="text-indigo-700 font-bold hover:text-white px-6 py-1 rounded-md transition-all duration-300 block">Blog</a></li>
-                        <li><a href="#" className="text-indigo-700 font-bold hover:text-white px-6 py-1 rounded-md transition-all duration-300 block">About</a></li>
+                        <li><a href="#" className="text-black hover:bg-white/75 px-6 py-1 rounded-md transition-all duration-300 block">Features</a></li>
+                        <li><a href="#" className="text-black hover:bg-white/75 px-6 py-1 rounded-md transition-all duration-300 block">Pricing</a></li>
+                        <li><a href="#" className="text-black hover:bg-white/75 px-6 py-1 rounded-md transition-all duration-300 block">Blog</a></li>
+                        <li><a href="#" className="text-black hover:bg-white/75 px-6 py-1 rounded-md transition-all duration-300 block">About</a></li>
                     </ul>
                     <button className={`${styles.mobileButton}`}>Get Started</button>
                 </div>
